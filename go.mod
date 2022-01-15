0db009be3cc0cd6da7d3c78b90a3817a7d1c64ba
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-15 21:02:34 +0700

    init

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/mriskyn/books-api.git
+
+go 1.17
+
+require github.com/gofiber/fiber v1.14.6
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/gofiber/fiber/v2 v2.24.0 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.31.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+)
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/mriskyn/books-api.git/book"
+	"github.com/mriskyn/books-api.git/database"
+	// _ "github.com/mriskyn/books-api.git/database/dialects/sqlite"
+	// "github.com/mriskyn/books-api.git/book"
+)
+
+func helloWorld(c *fiber.Ctx) {
+	c.Send("Hello world")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("api/v1/book", book.GetBooks)
+	app.Post("api/v1/book/", book.NewBook)
+	app.Get("api/v1/book/:id", book.GetBook)
+	app.Delete("api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase(){
+	var err error
+
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	fmt.Println("Database succesfully opened")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+
+	initDatabase()
+	defer database.DBConn.Close()
+
+
+	app.Get("/", helloWorld)
+	setupRoutes(app)
+
+	app.Listen("localhost:3333")
+}
